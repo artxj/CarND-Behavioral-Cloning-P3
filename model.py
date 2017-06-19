79cd35b73e58a93910dac285d9ef0bc8676cdacd
Artem Lagzdin <art.lagzdin@gmail.com>
2017-06-19 15:38:27 +0600

    - Run1 video (for previous commit model); - EarlyStopping callback; - Trying to change steering angle shift.

--- model.py
+++ model.py
@@ -2,12 +2,13 @@
 import csv
 import cv2
 from keras import regularizers
-#from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping
 from keras.models import Sequential, Model
 from keras.layers import Activation, Conv2D, Cropping2D, Dense, Dropout, Flatten, Lambda
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import random
 import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -24,6 +25,18 @@ def flip_image(image, angle):
     return np.fliplr(image), -angle
     #return cv2.flip(image, flipCode=1), -angle
 
+def apply_shadows(image, alpha=0.7):
+    overlay = image.copy()
+    output = image.copy()
+    img_height = image.shape[0]
+    img_width = image.shape[1]
+    top_point = random.random()
+    bottom_point = random.random()
+    pts = np.array( [[[0, 0], [int(top_point * img_width), 0], [int(bottom_point * img_width), img_height], [0, img_height]]], dtype=np.int32)
+    cv2.fillPoly(overlay, pts, (0, 0, 0))
+    cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+    return output
+
 def read_image_data(file_path, angle, shift=0.0):
     image = cv2.imread(file_path)
     return image, (angle + shift)
@@ -39,10 +52,12 @@ def samples_generator(samples, batch_size=32, shift=0.2):
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
 
-            images = []
-            angles = []
+            #images = []
+            #angles = []
             # speeds = []
             for batch_sample in batch_samples:
+                #new_images = []
+                #new_angles = []
                 center_image, center_angle = read_image_data(convert_path(batch_sample[0]), float(batch_sample[3]))
                 left_image, left_angle = read_image_data(convert_path(batch_sample[1]), center_angle, shift)
                 right_image, right_angle = read_image_data(convert_path(batch_sample[2]), center_angle, -shift)
@@ -51,18 +66,21 @@ def samples_generator(samples, batch_size=32, shift=0.2):
                     continue
 
                 # speed = float(batch_sample[6])
-
+                #new_images = [center_image, left_image, right_image]
+                #new_angles = [center_angle, left_angle, right_angle]
                 images = images + [center_image, left_image, right_image]
                 angles = angles + [center_angle, left_angle, right_angle]
-                # speeds = speeds + [speed, 0.75 * speed, 0.75 * speed]
 
                 flipped_center_image, flipped_center_angle = flip_image(center_image, center_angle)
                 flipped_left_image, flipped_left_angle = flip_image(left_image, left_angle)
                 flipped_right_image, flipped_right_angle = flip_image(right_image, right_angle)
 
+                #new_images = new_images + [flipped_center_image, flipped_left_image, flipped_right_image]
                 images = images + [flipped_center_image, flipped_left_image, flipped_right_image]
                 angles = angles + [flipped_center_angle, flipped_left_angle, flipped_right_angle]
-                # speeds = speeds + [speed, 0.75 * speed, 0.75 * speed]
+
+                #for new_image in new_images:
+
 
             X_train = np.array(images)
             y_train = np.array(angles)
@@ -117,18 +135,27 @@ def show_histogram(samples, shift=0.2):
     plt.hist(y_data, bins=np.unique(y_data))
     plt.show()
 
+'''
+path = './data/data1_1/IMG/center_2017_06_11_18_32_25_404.jpg'
+image = cv2.imread(path)
+image = apply_shadows(image)
+plt.imshow(image)
+plt.show()
+'''
+
 log_paths = [ './data/data1_1/driving_log.csv',\
-    './data/data1_2/driving_log.csv', './data/data1_3/driving_log.csv',\
-    './data/data1_4/driving_log.csv', './data/data1_5/driving_log.csv',\
-    './data/data1_6/driving_log.csv', './data/data1_7/driving_log.csv',\
-    './data/data2_1/driving_log.csv', './data/data2_2/driving_log.csv',\
+    './data/data1_2/driving_log.csv', './data/data1_3/driving_log.csv',
+    './data/data1_4/driving_log.csv', './data/data1_5/driving_log.csv',
+    './data/data1_6/driving_log.csv', './data/data1_7/driving_log.csv',
+    './data/data2_1/driving_log.csv', './data/data2_2/driving_log.csv',
     './data/data2_3/driving_log.csv', './data/data2_4/driving_log.csv',
-    './data/data2_5/driving_log.csv' ]
+    './data/data2_5/driving_log.csv', './data/data2_6/driving_log.csv',
+    './data/data2_7/driving_log.csv' ]
 model_path = './model/model.h5'
 nb_epoch = 30
 image_shape = (160, 320, 3)
 batch_size = 64
-angle_shift = 0.22
+angle_shift = 0.15
 
 print('Loading samples...')
 
@@ -153,7 +180,7 @@ model = build_model(cropping=((60, 25), (0, 0)), input_shape=image_shape)
 model.compile(loss='mse', optimizer='adam')
 history = model.fit_generator(train_generator, steps_per_epoch=nb_train_samples // batch_size,\
     validation_data=validation_generator, validation_steps=nb_validation_samples // batch_size,\
-    #callbacks=EarlyStopping(monitor='val_loss', min_delta=.0, patience=2),\
+    callbacks=[EarlyStopping(monitor='val_loss', min_delta=.0, patience=1)],\
     epochs=nb_epoch)
 
 # plot_history(history)
